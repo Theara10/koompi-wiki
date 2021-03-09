58edde2b01d05fb59824559a6e39aeb2cb2b1dc3
Theara10 <thearatheng10@gmail.com>
2021-03-09 23:11:42 +0700

    github link

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,11 +1,36 @@
 import React from "react";
 import { Anchor } from "antd";
+
+import { DownOutlined, GithubOutlined } from "@ant-design/icons";
 const { Link } = Anchor;
+
+const img = require("../lib/img/github_logo.png");
 
 function Preview() {
   return (
     <div className="preview">
-      <Anchor>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <div className="select-lang">
+          <p>KH</p>
+          <DownOutlined style={{ fontSize: 10 }} />
+        </div>
+        <div style={{ marginLeft: 20, display: "flex" }}>
+          <GithubOutlined style={{ fontSize: 25 }} />
+          <div
+            style={{
+              height: 25,
+              width: 60,
+              border: "1px solid #262e3c",
+              borderRadius: 3,
+              marginLeft: 5,
+            }}
+          >
+            <p style={{ textAlign: "center" }}>Github</p>
+          </div>
+        </div>
+      </div>
+
+      <Anchor className="anchor">
         <Link href="#page1" title="Welcome" />
         <Link href="#page2" title="Install" />
         <Link href="#page3" title="Uninstall Applications" />
